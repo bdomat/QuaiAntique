27f5f28823ee9bbb1a6a66fea936c3fcb7ab0418
Bertrand Domat <bertrand.domat@gmail.com>
2023-04-25 16:48:08 +0200

    dynamic rendering of image gallery

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ImagesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ImagesRepository $imagesRepository): Response
     {
+        $images = $imagesRepository->findAll();
         return $this->render('/front/index.html.twig', [
             'controller_name' => 'HomeController',
+            'images' => $images
         ]);
     }
 }
